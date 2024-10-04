bbda646248f43b174e4e8171ba053f3b75de5d78
matias-rapa-sinis <matias.rapaport@sinis.com.ar>
2024-10-04 08:58:51 -0300

    Bug arreglado. El problema es que traje los Headers del proyecto de plantillas original que tenia una variable de entorno que obviamente en este proyecto no existe.

--- app/routes/vista.tsx
+++ app/routes/vista.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "@remix-run/react";
+import { useLoaderData, useParams } from "@remix-run/react";
 import NavbarComponent from "~/components/Navbar";
 import { json } from "@remix-run/react";
 import type { LoaderFunctionArgs} from "@remix-run/node";
@@ -15,20 +15,21 @@ import type { LoaderFunctionArgs} from "@remix-run/node";
 export const loader = async ({
     params,
 }: LoaderFunctionArgs) => {
-    console.log(params.id);
+
     try{
         const response = await fetch(`https://apptesting.leiten.dnscheck.com.ar/ContentSettings/GetMenu/IdVista/1/IdMenu/2`, {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': process.env.REACT_APP_TESTING_ACCESS
+                'Authorization': '12345'
             }
         });
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        return json({ user: data });
+        console.log(data)
+        return json({ data : data });
 
     }catch(error){
         console.error(`Error fetching url :`, error);
@@ -42,7 +43,7 @@ export const loader = async ({
 
 export default function templeteBasic() {
     const { idVista, idMenu } = useParams();
-
+    const {data} = useLoaderData();
     return (
         <div className="container-fluid">
             <NavbarComponent  />
@@ -50,6 +51,16 @@ export default function templeteBasic() {
                 <div className="col">
                     Recibi este idVista : {idVista || "vacio"}
                     Recivi este idMenu : {idMenu || "vacio"}
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <h3> {data.Title} </h3>
+                    <pre>
+                        {data.MenuItems.map((item)=>(
+                            <p> {item.Title} </p>
+                        ))}
+                    </pre>
                 </div>
             </div>
         </div>
