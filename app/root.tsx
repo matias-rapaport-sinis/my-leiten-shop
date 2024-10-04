1d6e7c03aed5f324c6c5969d9911331b1dbe9891
matias-rapa-sinis <matias.rapaport@sinis.com.ar>
2024-10-04 07:31:15 -0300

    Elimine Tailwind, y agregue Bootstrap como recurso externo que se importa al cargar la pagina. Esto esta mal desde un punto de vista tecnico, ya que hace mas lento la carga de la pagina, pero en esta iteración no agrega valor el hacer la intalacion de Bootstrap pensando en que sea escalable. Aun mas que no esta decifido el estilo que utilizara los templates.

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,6 @@ import {
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -20,6 +19,11 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+    crossOrigin: "anonymous",
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -35,6 +39,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossOrigin="anonymous"></script>
       </body>
     </html>
   );
