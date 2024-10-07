e71326bf401a1df77a5574b5be2435e8be7b5785
matias-rapa-sinis <matias.rapaport@sinis.com.ar>
2024-10-07 13:20:01 -0300

    Selecctor the rango funciona, pero no tan bien como quisiera

--- app/components/SeleccionDeRango.tsx
+++ app/components/SeleccionDeRango.tsx
@@ -3,7 +3,21 @@ import { useState } from "react";
 
 export default function SeleccionDeRango({ nombre, opciones, index, handleChangeAdd, handleChangeRemove }) {
 
-    
+    const [maxValue, setMaxValue] = useState(100000);
+    const [minValue, setMinValue] = useState(0);
+
+    const handleOnChangeMaxValue = (e, keySelected) => {
+        handleChangeRemove(keySelected);
+        handleChangeAdd({ key: keySelected, value: e.target.value });
+        setMaxValue(e.target.value);
+    }
+
+    const handleOnChangeMinValue = (e, keySelected) => {
+        handleChangeRemove(keySelected);
+        handleChangeAdd({ key: keySelected, value: e.target.value });
+        setMinValue(e.target.value);
+    }
+
 
     return (
         <div className="container" key={`seleccionUnica-${index}`}>
@@ -15,22 +29,30 @@ export default function SeleccionDeRango({ nombre, opciones, index, handleChange
             <div className="row">
                 <div className="col d-flex flex-column" >
                     <label>
-                        {opciones["$values"][1].texto}
+                        {opciones["$values"][1].texto} : {minValue}
                         <input
-                            type="number"
+                            type="range"
+                            className="form-range"
+                            min="0"
+                            max={maxValue}
+                            step="1"
+                            id="customRange3"
                             onChange={(e) => (handleOnChangeMinValue(e, opciones["$values"][1].id))}
-                            name={`grupo-${opciones["$id"]}`}
                         />
                     </label>
                     <label>
-                        {opciones["$values"][0].texto}
+                        {opciones["$values"][0].texto} : {maxValue}
                         <input
-                            type="number"
-                            onBlur={(e) => (handleOnChangeMaxValue(e, opciones["$values"][0].id))}
-                            name={`grupo-${opciones["$id"]}`}
+                            type="range"
+                            className="form-range"
+                            min={minValue}
+                            max="10000"
+                            step="1"
+                            id="customRange3"
+                            onChange={(e) => (handleOnChangeMaxValue(e, opciones["$values"][0].id))}
                         />
                     </label>
-                    </div>
+                </div>
             </div>
 
         </div>
--- app/routes/_templateBase.vista.$idVista.menu.$idMenu.filtros.tsx
+++ app/routes/_templateBase.vista.$idVista.menu.$idMenu.filtros.tsx
@@ -5,17 +5,18 @@ import SeleccionMultiple from "~/components/SeleccionMultiple";
 import SeleccionUnica from "~/components/SeleccionUnica";
 import { getAtributosLoader } from "~/loaders/getAtributosLoader";
 
-export const loader = getAtributosLoader; 
+export const loader = getAtributosLoader;
 
-export default function Temple(){
+
+export default function Temple() {
     const { data } = useLoaderData<{ data }>();
-    const {idVista, idMenu, idProduct, filters} = useParams();
+    const { idVista, idMenu, idProduct, filters } = useParams();
     const [filterSelected, setFilterSelected] = useState<{ key: string, value: any }[]>([]);
     const navigate = useNavigate();
 
 
     const updateNavigation = (filters: { key: string, value: any }[]) => {
-        const listOfFiltros = JSON.stringify(filters); 
+        const listOfFiltros = JSON.stringify(filters);
         navigate(`productos/${idProduct}/${listOfFiltros}`);
     };
 
@@ -27,7 +28,7 @@ export default function Temple(){
             return newFilters;
         });
     };
-    
+
     const handleChangeRemove = (key: string) => {
         setFilterSelected(prev => {
             const newFilters = prev.filter(item => item.key !== key);
@@ -41,37 +42,36 @@ export default function Temple(){
         <div className="container-fluid">
             <div className="row">
                 <div className="col-3">
-                Esto es tus filtros     2
-                    {data["$values"].map((item, index)=>(
-                        item.accion === "SeleccionMultiple" ? 
-                        <SeleccionMultiple 
-                            key={`seleccionMultiple-${index}`} 
-                            index={index} 
-                            nombre={item["nombre"]} 
-                            opciones={item["opciones"]} 
-                            handleChangeAdd={handleChangeAdd}
-                            handleChangeRemove={handleChangeRemove}
+                    {data["$values"].map((item, index) => (
+                        item.accion === "SeleccionMultiple" ?
+                            <SeleccionMultiple
+                                key={`seleccionMultiple-${index}`}
+                                index={index}
+                                nombre={item["nombre"]}
+                                opciones={item["opciones"]}
+                                handleChangeAdd={handleChangeAdd}
+                                handleChangeRemove={handleChangeRemove}
                             />
-                        :
-                        (item.accion === "SeleccionUnica" ?
-                            <SeleccionUnica 
-                                key={`seleccionUnica-${index}`} 
-                                index={index} 
-                                nombre={item["nombre"]} 
-                                opciones={item["opciones"]} 
-                                handleChangeAdd={handleChangeAdd}
-                                handleChangeRemove={handleChangeRemove}
-                                />
                             :
-                            <SeleccionDeRango 
-                                key={`seleccionDeRango-${index}`} 
-                                index={index} 
-                                nombre={item["nombre"]} 
-                                opciones={item["opciones"]} 
-                                handleChangeAdd={handleChangeAdd}
-                                handleChangeRemove={handleChangeRemove}
+                            (item.accion === "SeleccionUnica" ?
+                                <SeleccionUnica
+                                    key={`seleccionUnica-${index}`}
+                                    index={index}
+                                    nombre={item["nombre"]}
+                                    opciones={item["opciones"]}
+                                    handleChangeAdd={handleChangeAdd}
+                                    handleChangeRemove={handleChangeRemove}
                                 />
-                        )
+                                :
+                                <SeleccionDeRango
+                                    key={`seleccionDeRango-${index}`}
+                                    index={index}
+                                    nombre={item["nombre"]}
+                                    opciones={item["opciones"]}
+                                    handleChangeAdd={handleChangeAdd}
+                                    handleChangeRemove={handleChangeRemove}
+                                />
+                            )
                     ))}
                 </div>
                 <div className="col-9">
